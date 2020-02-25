81fcd5ba5db2a17e591afe00499f9f05207b23e3
Kasperovich <zhenkasp@mail.ru>
2020-02-25 13:19:37 +0300

    add user/show ,same improvements

--- hello/app/controllers/users_controller.rb
+++ hello/app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+before_filter :check_current_user, only: [:show]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(current_user)
+  end
+
+  def check_current_user
+    @user = User.find(params[:id])
+    redirect_to user_path[:id] unless current_user == @user
+  end
 end
--- hello/config/routes.rb
+++ hello/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   get 'about' => 'about#index'
-
+  resources :users, only: :show
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
