6c5ecd86a766e0e6dc79a2a28149536fa0951af7
Kasperovich <zhenkasp@mail.ru>
2020-02-28 17:20:21 +0300

    we need more images

--- hello/db/schema.rb
+++ hello/db/schema.rb
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 20200227185037) do
     t.datetime "image_updated_at"
   end
 
+  create_table "user_profiles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
